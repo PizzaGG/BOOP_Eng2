2b7964cd3f56e1aabe89f05abb6b22f6b3255d96
Adawilcifran <adawilcifran@live.com>
2018-06-04 18:40:36 -0300

    Correção do Beans.xml e na declaração das anotações.

--- src/main/java/br/com/boop/dao/UsuarioDao.java
+++ src/main/java/br/com/boop/dao/UsuarioDao.java
@@ -3,38 +3,38 @@ package br.com.boop.dao;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
 import br.com.boop.model.Usuario;
-import br.com.boop.model.UsuarioLogado;
 
 @RequestScoped
-public class UsuarioDao {
+public class UsuarioDao  {
 
-private final EntityManager manager;
-	
+	protected final EntityManager manager;
+
 	@Inject
 	public UsuarioDao(EntityManager manager) {
 		this.manager = manager;
 	}
-	
+
 	@Deprecated
-	public UsuarioDao() {
+	protected UsuarioDao() {
 		this(null);
 	}
 
 	public boolean existe(Usuario usuario) {
-		return !manager.createQuery("select u from Usuario u where u.nome = "
-			+ ":nome and u.matricula = :matricula", Usuario.class)
-			.setParameter("nome", usuario.getNome())
-			.setParameter("matricula", usuario.getMatricula())
-			.getResultList().isEmpty();
+		return !manager
+				.createQuery("select u from Usuario u where u.nome = " + ":nome and u.matricula = :matricula",
+						Usuario.class)
+				.setParameter("nome", usuario.getNome()).setParameter("matricula", usuario.getMatricula())
+				.getResultList().isEmpty();
 	}
 
 	public void salva(Usuario usuario) {
 		manager.persist(usuario);
 	}
-	
+
 	public void atualizar(Usuario usuario) {
-		//TODO fazer o corpo
+		// TODO fazer o corpo
 	}
 
 }
