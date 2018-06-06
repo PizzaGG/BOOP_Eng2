309dc44c8dc75901ed84c358ec554e35c6e1912f
Adawilcifran <adawilcifran@live.com>
2018-06-06 12:01:33 -0300

    Pequena edição no metodo auth

--- src/main/java/br/com/boop/controller/LoginController.java
+++ src/main/java/br/com/boop/controller/LoginController.java
@@ -4,12 +4,13 @@ package br.com.boop.controller;
 import javax.inject.Inject;
 
 import br.com.boop.dao.UsuarioDao;
-import br.com.boop.model.Usuario;
+import br.com.boop.model.BoopMessage;
 import br.com.boop.model.UsuarioLogado;
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.validator.I18nMessage;
+import br.com.caelum.vraptor.validator.Severity;
 import br.com.caelum.vraptor.validator.Validator;
 
 @Controller
@@ -19,27 +20,32 @@ public class LoginController {
 	private final Validator validator;
 	private final Result result;
 	private final UsuarioLogado usuarioLogado;
+	private final UsuarioDao usuarioDao;
 
 	@Inject
-	public LoginController(UsuarioDao dao, Validator validator, Result result, UsuarioLogado _usuarioLogado) {
+	public LoginController(UsuarioDao dao, Validator validator, Result result,UsuarioLogado usuarioLogado, UsuarioDao usuarioDao) {
 		this.dao = dao;
-		this.usuarioLogado = _usuarioLogado;
 		this.result = result;
 		this.validator = validator;
+		this.usuarioLogado = usuarioLogado;
+		this.usuarioDao = usuarioDao;
 	}
 
 	@Deprecated
 	public LoginController() {
-		this(null, null, null, null);
+		this(null, null, null, null, null);
 	}
 
 	@Post("/auth")
-	public void autentica(Usuario usuario) {
-		if (!dao.existe(usuario)) {
-			validator.add(new I18nMessage("login", "login.invalido"));
-			validator.onErrorUsePageOf(HomeController.class).index();
+	public void autentica(String user, String pass) {
+		if (usuarioLogado != null) {
+			MessagesController.addMessage(new BoopMessage("has.logged.user.tittle", "has.loggued.user.message", Severity.WARN));
+			result.redirectTo(LoginController.class).home();
+			return;
 		}
-		usuarioLogado.setUsuario(usuario);
-		result.redirectTo(HomeController.class).index();
+	}
+	
+	@Get("/")
+	public void home() {
 	}
 }
