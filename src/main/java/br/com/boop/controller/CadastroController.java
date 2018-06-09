b024dc4ee78b3a1c9e27d8ddb6b93df825db8521
pizzagg <pizzagg@p.renato>
2018-06-08 23:04:48 -0300

    Adicionado método de cadastro e construtores

--- src/main/java/br/com/boop/controller/CadastroController.java
+++ src/main/java/br/com/boop/controller/CadastroController.java
@@ -1,8 +1,56 @@
 package br.com.boop.controller;
 
+import javax.inject.Inject;
+import javax.validation.Valid;
+
+import br.com.boop.dao.LivroDao;
+import br.com.boop.model.BoopMessage;
+import br.com.boop.model.Livro;
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.Message;
+import br.com.caelum.vraptor.validator.Severity;
+import br.com.caelum.vraptor.validator.Validator;
 
 @Controller
 public class CadastroController {
+
+	private final LivroDao livroDao;
+	private final Validator validator;
+	private final Result result;
+	
+	@SuppressWarnings("unused")
+	@Deprecated
+	public CadastroController() {
+		this(null, null, null);
+	}
+	
+	@Inject
+	public CadastroController(Result _result, Validator _validator, LivroDao _livroDao) {
+		this.validator = _validator;
+		this.result = _result;
+		this.livroDao = _livroDao;
+	}
+	
+	@Get("/cadastro")
+	public void cadastrarLivro() {
+	}
+	
+	@Post("/cadastrar")
+	public void cadastrar(@Valid Livro livro) {
+		if (validator.hasErrors()) {
+			validator.onErrorForwardTo(this).cadastrarLivro();
+			for (Message msg : validator.getErrors()) {
+				MessagesController.addMessage(new BoopMessage("user.update.error.title", msg.getMessage(), msg.getSeverity()));
+			}
+			return;
+		}
+		livroDao.salvar(livro);
+		MessagesController.addMessage(new BoopMessage("user.update.sucess.title", "user.update.sucess.message", Severity.INFO));
+		result.redirectTo(HomeController.class).home();
+		return;
+	}
 	
 }
