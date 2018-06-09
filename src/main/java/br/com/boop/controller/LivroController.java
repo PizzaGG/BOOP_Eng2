df149d68668152b8442340c551d5a7b1c122b554
pizzagg <pizzagg@p.renato>
2018-06-08 23:07:32 -0300

    Adicionado método de update

--- src/main/java/br/com/boop/controller/LivroController.java
+++ src/main/java/br/com/boop/controller/LivroController.java
@@ -7,6 +7,7 @@ import br.com.boop.dao.LivroDao;
 import br.com.boop.model.BoopMessage;
 import br.com.boop.model.Livro;
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.validator.Message;
@@ -31,6 +32,10 @@ public class LivroController {
 		this(null, null, null);
 	}
 	
+	@Get("/livro")
+	public void livro(){
+	}
+	
 	@Post("/cadastrar")
 	public void cadastrar(Livro livro) {
 		if (validator.hasErrors()) {
@@ -44,4 +49,25 @@ public class LivroController {
 		MessagesController.addMessage(new BoopMessage("book.register.sucess.title", "book.register.sucess.message", Severity.INFO));
 		result.redirectTo(HomeController.class).home();
 	}
+	
+	@Post("/atualizar")
+	public void atualizar(Livro livro) {
+		if (validator.hasErrors()) {
+			validator.onErrorForwardTo(HomeController.class).home();
+			for (Message msg : validator.getErrors()) {
+				MessagesController.addMessage(new BoopMessage("book.register.error.title", msg.getMessage(), msg.getSeverity()));
+			}
+			return;
+		}
+		livroDao.atualizar(livro);
+		MessagesController.addMessage(new BoopMessage("book.register.sucess.title", "book.register.sucess.message", Severity.INFO));
+		result.redirectTo(HomeController.class).home();
+	}
+	
+	public void deletar(Long idLivro) {
+		livroDao.deletar(idLivro);
+		MessagesController.addMessage(new BoopMessage("book.register.sucess.title", "book.register.sucess.message", Severity.INFO));
+		result.redirectTo(HomeController.class).home();
+	}
+	
 }
