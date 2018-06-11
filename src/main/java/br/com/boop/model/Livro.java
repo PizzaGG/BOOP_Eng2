36d24ee4fa5207ed4fc6695c9cc78f535bc04cc3
Adawilcifran <adawilcifran@live.com>
2018-06-11 20:40:09 -0300

    Invalidação de todas as validações dos atributos.

--- src/main/java/br/com/boop/model/Livro.java
+++ src/main/java/br/com/boop/model/Livro.java
@@ -4,13 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Livro {
@@ -19,44 +12,44 @@ public class Livro {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
-	@NotNull(message = "{not.null.attribute}")
-	@NotEmpty(message = "{not.empty.attribute}")
-	@NotBlank(message = "{not.blank.attribute}")
+//	@NotNull(message = "{not.null.attribute}")
+//	@NotEmpty(message = "{not.empty.attribute}")
+//	@NotBlank(message = "{not.blank.attribute}")
 	private String titulo;
 	
-	@NotNull(message = "{not.null.attribute}")
-	@NotEmpty(message = "{not.empty.attribute}")
-	@NotBlank(message = "{not.blank.attribute}")
-	@Size(min = 10, max = 100, message = "{invalid.size.attribute}")
+//	@NotNull(message = "{not.null.attribute}")
+//	@NotEmpty(message = "{not.empty.attribute}")
+//	@NotBlank(message = "{not.blank.attribute}")
+//	@Size(min = 10, max = 100, message = "{invalid.size.attribute}")
 	private String subtitulo;
 	
-	@NotNull(message = "{not.null.attribute}")
-	@NotEmpty(message = "{not.empty.attribute}")
-	@NotBlank(message = "{not.blank.attribute}")
+//	@NotNull(message = "{not.null.attribute}")
+//	@NotEmpty(message = "{not.empty.attribute}")
+//	@NotBlank(message = "{not.blank.attribute}")
 	private Integer ano;
 	
-	@NotNull(message = "{not.null.attribute}")
-	@NotEmpty(message = "{not.empty.attribute}")
-	@NotBlank(message = "{not.blank.attribute}")
+//	@NotNull(message = "{not.null.attribute}")
+//	@NotEmpty(message = "{not.empty.attribute}")
+//	@NotBlank(message = "{not.blank.attribute}")
 	private Short edicao;
 	
-	@NotNull(message = "{not.null.attribute}")
-	@NotEmpty(message = "{not.empty.attribute}")
-	@NotBlank(message = "{not.blank.attribute}")
+//	@NotNull(message = "{not.null.attribute}")
+//	@NotEmpty(message = "{not.empty.attribute}")
+//	@NotBlank(message = "{not.blank.attribute}")
 	private String isbn;
 	
-	@OneToOne
-	@NotNull(message = "{not.null.attribute}")
-	private @Valid Usuario proprietario;
+//	@OneToOne
+//	@NotNull(message = "{not.null.attribute}")
+	private String proprietario;
 	
 	//@ManyToOne
-	@NotNull(message = "{not.null.attribute}")
-	private @Valid String autor;
+//	@NotNull(message = "{not.null.attribute}")
+	private String autor;
 	
 	public Livro() {
 	}
 	
-	public Livro(Long id, String titulo, String subtitulo, Integer ano, Short edicao, String isbn, Usuario proprietario,
+	public Livro(Long id, String titulo, String subtitulo, Integer ano, Short edicao, String isbn, String proprietario,
 			String autor) {
 		super();
 		this.id = id;
@@ -97,7 +90,7 @@ public class Livro {
 		this.isbn = isbn;
 	}
 
-	public void setProprietario(Usuario proprietario) {
+	public void setProprietario(String proprietario) {
 		this.proprietario = proprietario;
 	}
 
@@ -123,7 +116,7 @@ public class Livro {
 	public String getAutor() {
 		return autor;
 	}
-	public Usuario getProprietario() {
+	public String getProprietario() {
 		return proprietario;
 	}
 }
