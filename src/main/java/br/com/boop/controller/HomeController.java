0e1aaca06d2d15e3a62544a0674a279c3d8565d2
pizzagg <pizzagg@p.renato>
2018-06-12 23:23:10 -0300

    Adicionado metodo de logout

--- src/main/java/br/com/boop/controller/HomeController.java
+++ src/main/java/br/com/boop/controller/HomeController.java
@@ -1,12 +1,34 @@
 package br.com.boop.controller;
 
+import javax.inject.Inject;
+
+import br.com.boop.model.UsuarioLogado;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
 
 @Controller
 public class HomeController {
+	
+	private Result result;
+
+	@Inject
+	public HomeController(Result result) {
+		this.result = result;
+	}
+	
+	@Deprecated
+	public HomeController() {
+		this(null);
+	}
 
 	@Get("/home")
-	public void home() {
+	public void home(){
+	}
+	
+	@Get("/sair")
+	public void sair() {
+		UsuarioLogado.setUsuario(null);
+		result.redirectTo(LoginController.class).login();
 	}
 }
