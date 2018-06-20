8aac82ca011904ae437e3b5be1a6e6c9e038586c
pizzagg <pizzagg@p.renato>
2018-06-20 13:54:44 -0300

    TrocaDao criado

---
+++ src/main/java/br/com/boop/dao/TrocaDao.java
@@ -0,0 +1,35 @@
+package br.com.boop.dao;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.boop.model.Troca;
+
+@RequestScoped
+public class TrocaDao {
+
+private final EntityManager em;
+	
+	@Inject
+	public TrocaDao (EntityManager em)	{
+		this.em=em;
+	}
+	
+	protected TrocaDao () {
+		this(null);
+	}
+	
+	public void salvar (Troca troca) {
+		em.persist(troca);
+	}
+	
+	public List<Troca> listarTrocasUsuario (String username){
+		List<Troca> lista = em.createQuery("select t from Troca t where t.originario = :username", Troca.class)
+				.setParameter("username", username).getResultList();
+		return lista;
+	}
+	
+}
