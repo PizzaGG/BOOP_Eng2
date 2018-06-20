a66c71c49999948e2d85193a73c8337b666178fd
pizzagg <pizzagg@p.renato>
2018-06-20 13:53:14 -0300

    Adicionados setters e cascades

--- src/main/java/br/com/boop/model/Troca.java
+++ src/main/java/br/com/boop/model/Troca.java
@@ -1,8 +1,13 @@
 package br.com.boop.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 import br.com.boop.model.enumerables.Status;
 
@@ -10,15 +15,19 @@ import br.com.boop.model.enumerables.Status;
 public class Troca {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@OneToOne
+	@Cascade(CascadeType.ALL)
 	private Usuario originario;
 	
 	@OneToOne
+	@Cascade(CascadeType.ALL)
 	private Usuario destinatario;
 	
 	@OneToOne
+	@Cascade(CascadeType.ALL)
 	private Livro livroSolicitado;
 	
 	private Status statusDoPedido;
@@ -34,6 +43,22 @@ public class Troca {
 	public Troca() {
 	}
 	
+	public void setOriginario(Usuario originario) {
+		this.originario = originario;
+	}
+
+	public void setDestinatario(Usuario destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public void setLivroSolicitado(Livro livroSolicitado) {
+		this.livroSolicitado = livroSolicitado;
+	}
+
+	public void setStatusDoPedido(Status statusDoPedido) {
+		this.statusDoPedido = statusDoPedido;
+	}
+
 	public Long getId() {
 		return id;
 	}
