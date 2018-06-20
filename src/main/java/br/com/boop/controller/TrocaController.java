ee2322e65d7cce6db387882594ad1b04fd8f3fa2
pizzagg <pizzagg@p.renato>
2018-06-20 14:25:03 -0300

    Adicionados metodos da logica de trocas

--- src/main/java/br/com/boop/controller/TrocaController.java
+++ src/main/java/br/com/boop/controller/TrocaController.java
@@ -44,9 +44,6 @@ public class TrocaController {
 	
 	@Post("/novaTroca")
 	public void novaTroca(Long userOrig, Long userDest, Long livro) {
-		System.out.println(livro);
-		System.out.println(userDest);
-		System.out.println(userOrig);
 		Troca t = new Troca();
 		t.setDestinatario(usuarioDao.busca(userDest));
 		t.setOriginario(usuarioDao.busca(userOrig));
@@ -55,4 +52,24 @@ public class TrocaController {
 		trocaDao.salvar(t);
 		result.redirectTo(this).troca();
 	}
+	
+	public void cancelarTroca(Troca t) {
+		t.setStatusDoPedido(Status.CANCELADO);
+		trocaDao.atualizar(t);
+	}
+	
+	public void aceitarTroca(Troca t) {
+		t.setStatusDoPedido(Status.AGUARDANDO_CONFIRMACAO_AMBOS);
+		trocaDao.atualizar(t);
+	}
+	
+	public void confirmarTroca(Troca t) {
+		if(t.getStatusDoPedido() == Status.AGUARDANDO_CONFIRMACAO_AMBOS) {
+			t.setStatusDoPedido(Status.AGUARDANDO_CONFIRMACAO_UNICA);
+		} else if(t.getStatusDoPedido() == Status.AGUARDANDO_CONFIRMACAO_UNICA) {
+			t.setStatusDoPedido(Status.APROVADO);
+		}
+		trocaDao.atualizar(t);
+	}
+	
 }
